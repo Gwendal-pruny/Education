e2f13965d3e404c929dedfaa4f7c79508611555d
Zerio <gwendal@squadeasy.com>
2021-10-25 12:32:16 +0200

    TextAdventure add spell

--- Cours_Esiee-it/Projet proffesionalisation/python-course/exercices/textadventure/textadventure.py
+++ Cours_Esiee-it/Projet proffesionalisation/python-course/exercices/textadventure/textadventure.py
@@ -1,6 +1,7 @@
 """
 Cours "Introduction 2" - Exercice "Text Adventure"
 """
+import yaml
 
 CHARACTERE = {
     "NAME"
@@ -12,37 +13,37 @@ CHARACTERE = {
               ]
 }
 
-FIRST_SPELL_LIST_MAGE = [
-    "Air"
-    "Bois"
-    "Eau"
-    "Feu"
-    "Métal"
-    "Néant"
+SPELL_LIST_MAGE = [
+    "Air",
+    "Bois",
+    "Eau",
+    "Feu",
+    "Métal",
+    "Néant",
     "Terre"
 ]
 
-FIRST_SPELL_LIST_PALADIN = [
-    "Armes magique"
-    "Soins légers"
-    "Lumière divine"
+SPELL_LIST_PALADIN = [
+    "Armes-magique",
+    "Soins-légers",
+    "Lumière-divine",
     "Purification"
 ]
 
-FIRST_SPELL_LIST_DEMON = [
-    "Armes magique"
-    "Peau de fer"
-    "Bénédiction du sang"
-    "Rage"
-    "Destruction"
+SPELL_LIST_DEMON = [
+    "Armes-magique",
+    "Peau-de-fer",
+    "Bénédiction-du-sang",
+    "Rage",
+    "Destruction",
     "Némésis"
 ]
 
-FIRST_SPELL_LIST_ASSASSIN = [
-    "True Strick"
-    "Secret Weapon",
-    "Blade of Blood"
-    "Poison Strick"
+SPELL_LIST_ASSASSIN = [
+    "True-Strick",
+    "Secret-Weapon",
+    "Blade-of-Blood",
+    "Poison-Strick"
 ]
 
 FOOD = [
@@ -71,8 +72,112 @@ def proposer_actions(actions):
     # A remplir ici
     print(message)
     
-def proposer_observer(oberserver):
-    message ="│[Observer] "
+# def proposer_observer(oberserver):
+
+
+def lvlup(CLASS, LVL):
+
+    #for the first lvlup
+    if LVL == 1:
+        if CLASS == "MAGE":
+            print("│ Choissisez votre premier sort MAGE:")
+            print(*SPELL_LIST_MAGE)
+            SPELL_CHOOSE = str(input("CHOIX : "))
+
+            if SPELL_CHOOSE in SPELL_LIST_MAGE:
+                SPELL_LIST_MAGE.remove(SPELL_CHOOSE)
+                CHARACTERE["SPELL"].append(SPELL_CHOOSE)
+            else :
+                print("Invalide input - RETRY")
+
+        
+        if CLASS == "PALADIN":
+            print("│ Choissisez votre premier sort PALADIN:")
+            print(*SPELL_LIST_PALADIN)
+            SPELL_CHOOSE = str(input("CHOIX : "))
+
+            if SPELL_CHOOSE in SPELL_LIST_PALADIN:
+                SPELL_LIST_PALADIN.remove(SPELL_CHOOSE)
+                CHARACTERE["SPELL"].append(SPELL_CHOOSE)
+            else :
+                print("Invalide input - RETRY")
+
+        
+        if CLASS == "DEMON":
+            print("│ Choissisez votre premier sort DEMON:")
+            print(*SPELL_LIST_DEMON)
+            SPELL_CHOOSE = str(input("CHOIX : "))
+
+            if SPELL_CHOOSE in SPELL_LIST_DEMON:
+                SPELL_LIST_DEMON.remove(SPELL_CHOOSE)
+                CHARACTERE["SPELL"].append(SPELL_CHOOSE)
+            else :
+                print("Invalide input - RETRY")
+
+        if CLASS == "ASSASSIN":
+            print("│ Choissisez votre premier sort ASSASSIN:")
+            print(*SPELL_LIST_ASSASSIN)
+            SPELL_CHOOSE = str(input("CHOIX : "))
+
+            if SPELL_CHOOSE in SPELL_LIST_ASSASSIN:
+                SPELL_LIST_ASSASSIN.remove(SPELL_CHOOSE)
+                CHARACTERE["SPELL"].append(SPELL_CHOOSE)
+            else :
+                print("Invalide input - RETRY")
+
+
+    #for higher than lvl 1
+    else :
+        if CLASS == "MAGE":
+            print("│ Choissisez votre premier sort MAGE")
+            print(*SPELL_LIST_MAGE)
+            SPELL_CHOOSE = str(input("CHOIX : "))
+
+            if SPELL_CHOOSE in SPELL_LIST_MAGE:
+                SPELL_LIST_MAGE.remove(SPELL_CHOOSE)
+                CHARACTERE["SPELL"].append(SPELL_CHOOSE)
+            else :
+                print("Invalide input - RETRY")
+        if CLASS == "PALADIN":
+            print("│ Choissisez votre premier sort PALADIN")
+            print(*SPELL_LIST_PALADIN)
+            SPELL_CHOOSE = str(input("CHOIX : "))
+
+            if SPELL_CHOOSE in SPELL_LIST_PALADIN:
+                SPELL_LIST_PALADIN.remove(SPELL_CHOOSE)
+                CHARACTERE["SPELL"].append(SPELL_CHOOSE)
+            else :
+                print("Invalide input - RETRY")
+        if CLASS == "DEMON":
+            print("│ Choissisez votre premier sort DEMON ")
+            print(*SPELL_LIST_DEMON)
+            SPELL_CHOOSE = str(input("CHOIX : "))
+
+            if SPELL_CHOOSE in SPELL_LIST_DEMON:
+                SPELL_LIST_DEMON.remove(SPELL_CHOOSE)
+                CHARACTERE["SPELL"].append(SPELL_CHOOSE)
+            else :
+                print("Invalide input - RETRY")
+
+        if CLASS == "ASSASSIN":
+            print("│ Choissisez votre premier sort ASSASSIN ")
+            print(*SPELL_LIST_ASSASSIN)
+            SPELL_CHOOSE = str(input("CHOIX : "))
+
+            if SPELL_CHOOSE in SPELL_LIST_ASSASSIN:
+                SPELL_LIST_ASSASSIN.remove(SPELL_CHOOSE)
+                CHARACTERE["SPELL"].append(SPELL_CHOOSE)
+            else :
+                print("Invalide input - RETRY")
+
+def printCharacter():
+    print("┌─────────────── CHARACTERE ──────────────────────")
+    print(yaml.dump(CHARACTERE))
+    print("└─────────────── CHARACTERE ──────────────────────")
+
+
+
+
 
 # ********************************************************************************
 # INTRODUCTION
@@ -89,21 +194,19 @@ def intro():
     print("Commençons par créer ton personnage.")
     AGE = int(input("\nQuel âge as-tu ?\n\n\n\n"))
     NAME = str(input("\nQuel nom as-tu ?\n\n\n\n"))
-    CLASS = str(input("\nQuel class souhaitez vous ? 1 -> MAGE \n 2 -> PALADIN \n 3 -> DEMON \n 4 -> ASSASSIN \n 5 -> RAMDOM"))
+    CLASS = str(input("\nQuel class souhaitez vous ?\n 1 -> MAGE \n 2 -> PALADIN \n 3 -> DEMON \n 4 -> ASSASSIN \n 5 -> RAMDOM\n"))
 
-    
+
     # Afficher la liste des pouvoirs (avec leur position) et demander d'en choisir un
     CHARACTERE["LVL"] = 1 
-    
-    print("**"+(NAME)+ " à LVLUP au niveaux "+ str(CHARACTERE["LVL"]) +"**\n")
-    print(FIRST_SPELL_LIST_[CLASS])
-    SPELL_CHOOSE = int(input("Choissisez votre nouvelle compétance :"))
-    
+    print("** "+(NAME)+ " à LVLUP au niveaux "+ str(CHARACTERE["LVL"]) +" **\n")
+    lvlup(CLASS, LVL = CHARACTERE["LVL"])    
 
+    
     # Stocker le nom du pouvoir choisi dans le dictionnaire "personnage"
-    CHARACTERE["SPELL"].append(SPELL_CHOOSE)
     # Afficher tout le contenu (clé et valeur) du dictionnaire "personnage"
-    print(CHARACTERE)
+    printCharacter()
+        
     lieu_hall()
 
 # ********************************************************************************
@@ -111,23 +214,49 @@ def intro():
 # ********************************************************************************
 
 def lieu_hall():
+    LIEUX = "lieu_hall"
+    print(chr(27) + "[2J")
     print("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
     print("Tu es dans le hall d'entrée de l'école.")
     print("On peut aller à de nombreux endroits d'ici.")
 
     while True:
         print("┌────────────────────────────────────────")
-        proposer_actions(["quitter"]) # À compléter
-        proposer_lieux(["escalier"]) # À compléter
-        reponse = input("├─> ")
+        proposer_observer(["│1.[Esseyer de rentré dans les différentes pièces]"])
+        proposer_actions(["│2.[Quitter le hall]"]) # À compléter
+        proposer_lieux(["│3.[Prendre les escalier vers le niveaux supérieur]"]) # À compléter
+        reponse = input("[Choisir le nombre correspondant a votre choix]├─> ")
         print("└────────────────────────────────────────")
 
         # Gérer ici toutes les réponses possibles, qu'elles soient correctes ou non
 
 def lieu_premier_etage():
+    print(chr(27) + "[2J")
     print("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
     print("C'est le couloir du 1er étage.")
     print("On y trouve entre autres la classe 1-A.")
+
+
+# ********************************************************************************
+# SPACE OPTION
+# ********************************************************************************
+
+def SpaceOptions(LIEUX):
+    if SpaceOptions.lieux == "lieu_hall":
+        print("│1.[Acceuil] \n")
+        print("│2.[Bureau du proviseur] \n")
+        print("│3.[Infirmerie] \n")
+        print("│4.[BDE] \n")
+        print("│5.[Super Secret Door] \n")
+        print("|6.[Prendre les escalier vers le niveaux supérieur]")
+        print("|7.[Quitter le hall]") # À compléter
+        reponse = input("[Choisir le nombre correspondant a votre choix]├─> ")
+
+
+
+
+
+
 
 # ********************************************************************************
 # EXECUTION
