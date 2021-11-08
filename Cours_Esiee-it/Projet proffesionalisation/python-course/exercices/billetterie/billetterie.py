09f9dd30b4b0f6124baf339e09eb3346a717ced4
Zerio <gwendal@squadeasy.com>
2021-11-08 22:45:00 +0100

    Python exo FINISHED

--- Cours_Esiee-it/Projet proffesionalisation/python-course/exercices/billetterie/billetterie.py
+++ Cours_Esiee-it/Projet proffesionalisation/python-course/exercices/billetterie/billetterie.py
@@ -1,7 +1,7 @@
 """
 Cours "Introduction 1" - Exercice "Billetterie"
 """
-
+from simple_term_menu import TerminalMenu
 # Variables
 stations = {
     "Meinohama": 1.5,
@@ -27,78 +27,94 @@ station_start_name = None
 station_end_name = None
 distance = 0
 voie = 1
+conditionGare = True
+conditionBillet = True
 
-
+# escape sequences
+print(chr(27) + "[2J")
 
 # Introduction
-print("           /////// ")
-print("         ///       ")
-print("  //////////////   ")
-print("      ///          ")
-print("///////            ")
+print("・．━━━━━━━━━━━━━━━━━━━━━ † ━━━━━━━━━━━━━━━━━━━━━．・")
+print("          ╭━━━┳╮╱╭┳╮╭━┳╮╱╭┳━━━┳╮╭━┳━━━╮")
+print("          ┃╭━━┫┃╱┃┃┃┃╭┫┃╱┃┃╭━╮┃┃┃╭┫╭━╮┃")
+print("          ┃╰━━┫┃╱┃┃╰╯╯┃┃╱┃┃┃╱┃┃╰╯╯┃┃╱┃┃")
+print("          ┃╭━━┫┃╱┃┃╭╮┃┃┃╱┃┃┃╱┃┃╭╮┃┃╰━╯┃")
+print("          ┃┃╱╱┃╰━╯┃┃┃╰┫╰━╯┃╰━╯┃┃┃╰┫╭━╮┃")
+print("          ╰╯╱╱╰━━━┻╯╰━┻━━━┻━━━┻╯╰━┻╯╱╰╯")
+
 print("\nBienvenue sur la billetterie du métro municipal de Fukuoka.")
 
-# Questions à l'utilisateur
-print("\n--- Combien souhaitez-vous de billets à tarif plein ?")
-nb_billets_adulte = int(input("Billets : "))
+while True :
+    # Questions à l'utilisateur
+    print("\n-> Combien souhaitez-vous de billets à tarif plein ?")
+    terminal_menu = TerminalMenu(["0","1","2","3","4","Choisire le nombre"])
+    nb_billets_adulte = terminal_menu.show()
+    if nb_billets_adulte == 5 :
+        try:
+            nb_billets_adulte = int(input("Nombre de billets :"))
+            break
+        except ValueError:
+            print(chr(27) + "[2J")
+            print("Merci d'utiliser des nombre")
+            continue
+    print(nb_billets_adulte)
+    break
 
-print("\n--- Souhaitez-vous des billets à tarif réduit ? oui / non")
-has_reduit = str(input("oui / non : "))
+while True :
+    print("\n-> Souhaitez-vous des billets à tarif réduit et si oui combien ?")
+    terminal_menu = TerminalMenu(["oui","non"])
+    has_reduit = terminal_menu.show()
+    if has_reduit == 0 :
+        terminal_menu = TerminalMenu(["0","1","2","3","4","Choisire le nombre"])
+        nb_billets_reduice = terminal_menu.show()
+        if nb_billets_reduice == 5 :
+            try:
+                nb_billets_reduice = int(input("Nombre de billets :"))
+                break
+            except ValueError:
+                print(chr(27) + "[2J")
+                print("Merci d'utiliser des nombre")
+                continue
+        break
+    elif has_reduit == 1 :
+        nb_billets_reduice = 0
+        break
+        
+print(nb_billets_reduice)
 
-if has_reduit != False and has_reduit != "non" :
-    print("\n--- Combien des billets à tarif réduit souhaitez-vous ?")
-    nb_billets_reduice = int(input("Billets : "))
+if nb_billets_adulte == 0 :
+        if nb_billets_reduice == 0 :
+            print(chr(27) + "[2J")
+            print("Sans billets vous n'irez pas loin Sagisgi ! Réesseyer..")
+            conditionBillet = True
+        else :
+            conditionBillet = False
+else :
+    conditionBillet = False
 
+# escape sequences
+print(chr(27) + "[2J")        
+while conditionGare == True:
+    print("\nQuelle est votre station de départ ?")
+    terminal_menu = TerminalMenu(["Meinohama","Muromi","Fujisaki","Nishijin","Tojinmachi","Ohorikoen (Ohori Park)","Akasaka","Tenjin","Nakasu-Kawabata","Gion","Hakata","Higashi-Hie","Fukuokakuko(Airport)"])
+    station_start = terminal_menu.show()
+    station_start_name = (stations_names[station_start])
+    print("Départ : ")
+    print(station_start_name)
 
-
-print("////////////////// Liste des stations disponibles //////////////////")
-print("\nQuelle est votre station de départ ? (Indiquer le nombre associer a la gare)")
-print("0.Meinohama, 1.Muromi, 2.Fujisaki, 3.Nishijin, 4.Tojinmachi, 5.Ohorikoen (Ohori Park)")
-print("6.Akasaka, 7.Tenjin, 8.Nakasu-Kawabata, 9.Gion, 10.Hakata, 11.Higashi-Hie, 12.Fukuokakuko(Airport)")
-print("////////////////// ////////////////// //////////////////")
-station_start = int(input(" Choix numéro : "))
-station_start_name = (stations_names[station_start])
-print("Départ : ")
-print(station_start_name)
-
-print("////////////////// Liste des stations disponibles //////////////////")
-print("\nQuelle est votre station de d'arriver ? (Indiquer le nombre associer a la gare)")
-print("0.Meinohama, 1.Muromi, 2.Fujisaki, 3.Nishijin, 4.Tojinmachi, 5.Ohorikoen (Ohori Park)")
-print("6.Akasaka, 7.Tenjin, 8.Nakasu-Kawabata, 9.Gion, 10.Hakata, 11.Higashi-Hie, 12.Fukuokakuko(Airport)")
-print("////////////////// ////////////////// //////////////////")
-station_end = int(input("Choix numéro : "))
-station_end_name = (stations_names[station_end])
-print("Arriver : ")
-print(station_end_name)
+    print("\nQuelle est votre station de d'arriver ?")
+    terminal_menu = TerminalMenu(["Meinohama","Muromi","Fujisaki","Nishijin","Tojinmachi","Ohorikoen (Ohori Park)","Akasaka","Tenjin","Nakasu-Kawabata","Gion","Hakata","Higashi-Hie","Fukuokakuko(Airport)"])
+    station_end = terminal_menu.show()
+    station_end_name = (stations_names[station_end])
+    print("Arriver : ")
+    print(station_end_name)
+    if station_end == station_start:
+        print("Vous êtes déjà a destination.. Réesseyer en choissiant une gare d'arriver différente de celle de départ baka..")
+    else :
+        conditionGare = False
 
 if stations_names[station_start:station_end] == [] :
-    if stations_names[station_end:station_start] == [] :
-        print("INVALIDE TRY AGAIN")
-    else :
-        voie = 2
-
-
-print("////////////////////////// ITINERAIRE //////////////////////////")
-print("Itinéraire : ")
-if voie == 2 :
-    print(stations_names[station_end:station_start])
-    print("reversed")
-else :
-    print(stations_names[station_start:station_end])
-    voie = 1
-print("//////////////////////////////////////////////////////////////// ")
-
-
-# Calculs de l'itinéraire
-print("////////////////////////// DISTANCE //////////////////////////")
-if voie == 2 :
-    distance = sum(stations_distances[station_end:station_start])
-else :
-    distance = sum(stations_distances[station_start:station_end])
-print("Vous allez parcourir")
-print(distance)
-print("kilomètres")
-print("//////////////////////////////////////////////////////////////")
+    voie = 2
 
 
 # Choix de la bonne zone tarifaire
@@ -112,26 +128,34 @@ if distance >= 7.1:
 if distance >= 11.1:
     tarif = 340
 
-
+# Calcul distance
+if voie == 2 :
+    distance = sum(stations_distances[station_end:station_start])
+else :
+    distance = sum(stations_distances[station_start:station_end])
 
 # Calcul du coût total
 
 Prix = ((nb_billets_adulte * tarif)+(nb_billets_reduice * (tarif / 2)))
 
+# escape sequences
+print(chr(27) + "[2J")
 
 # Affichage des détails du voyage et du tarif
 # Affichage de la voie du train à emprunter
-
-print("//////////////////////////////////////////////////////////////")
+print("          ╭━━━┳╮╱╭┳╮╭━┳╮╱╭┳━━━┳╮╭━┳━━━╮")
+print("          ┃╭━━┫┃╱┃┃┃┃╭┫┃╱┃┃╭━╮┃┃┃╭┫╭━╮┃")
+print("          ┃╰━━┫┃╱┃┃╰╯╯┃┃╱┃┃┃╱┃┃╰╯╯┃┃╱┃┃")
+print("          ┃╭━━┫┃╱┃┃╭╮┃┃┃╱┃┃┃╱┃┃╭╮┃┃╰━╯┃")
+print("          ┃┃╱╱┃╰━╯┃┃┃╰┫╰━╯┃╰━╯┃┃┃╰┫╭━╮┃")
+print("          ╰╯╱╱╰━━━┻╯╰━┻━━━┻━━━┻╯╰━┻╯╱╰╯")
+print("・．━━━━━━━━━━━━ ・ RÉCAPITULATIF ・ ━━━━━━━━━━━━．・")
 print("Itinéraire :")
 if voie == 2 :
     print(stations_names[station_end:station_start])
 else :
     print(stations_names[station_start:station_end])
-print("Distance a parcourir :")
-print(distance)
-print("Le montant a payer est de :")
-print(Prix)
-print("Votre train est sure la voix : \n Merci de votre achat et Bon voyage !")
-print(voie)
-print("//////////////////////////////////////////////////////////////")
+print("Distance a parcourir :", round(distance),"km")
+print("Le montant a payer est de :", round(Prix),"¥" "(Nombre billet adult :", nb_billets_adulte," pour ",(nb_billets_adulte * tarif),"¥ /","Nombre billet a tarif réduit :", nb_billets_reduice,"pour",nb_billets_reduice * (tarif / 2),"¥ )")
+print("Votre train est sure la voix :", voie, "\nMerci de votre achat et Bon voyage !")
+print("・．━━━━━━━━━━━━━━━━━━━━━ † ━━━━━━━━━━━━━━━━━━━━━．・")
