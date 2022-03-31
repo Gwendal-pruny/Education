91c6d5e95fcbca1804aa73892e0f5084f8316c2c
Zerio <gwendal@squadeasy.com>
2022-03-31 08:44:46 +0200

    Journar.Py Update

--- [2021-2022] BTS SIO SLAM/Info-M-Hubert-M/Exercices/journal/journal.py
+++ [2021-2022] BTS SIO SLAM/Info-M-Hubert-M/Exercices/journal/journal.py
@@ -4,22 +4,27 @@ Cours "Intermédiaire 1" - Exercice "Journal"
 
 import argparse
 import random
-
+from donnees import actricles
+from donnees import interviews
 # Import des variables nécessaires dans le module donnees
 
 class ElementJournal():
     # Variables communes aux articles et interviews
-
+    print(actricles)
+    print(interviews)
+    
     # Constructeur __init__ à écrire
-
+    
 class Article(ElementJournal):
     # Variable(s) spécifiques aux articles
+    
+    print(actricles.ElementJournal)
 
     # Constructeur __init__ à écrire
 
 class Interview(ElementJournal):
-    # Variable(s) spécifiques aux interviews
-
+    # Variable(s) spécifiques aux interviews    
+    print(interviews.ElementJournal)
     # Constructeur __init__ à écrire
 
 class Generateur():
@@ -49,15 +54,18 @@ class Generateur():
         # Ouverture et affichage des crédits
 
 if __name__ == "__main__":
-    # On crée l'instance de ce qui nous permets d'accepter des arugments à l'aide d'argparse
-    parser = argparse.ArgumentParser(description="Génère le journal du jour selon une date et une région.")
-    # On ajoute deux arguments positionnels, qui sont donc obligatoires
-    parser.add_argument("date", help="Date du journal à générer, sous le format 'annee-mois-jour'")
-    parser.add_argument("edition", help="Édition du journal à générer", choices=["national", "idf", "paca"])
-    # Argparse nous renvoie ensuite un objet contenant les valeurs des arguments
-    args = parser.parse_args()
+    
+    
+    
+    # # On crée l'instance de ce qui nous permets d'accepter des arugments à l'aide d'argparse
+    # parser = argparse.ArgumentParser(description="Génère le journal du jour selon une date et une région.")
+    # # On ajoute deux arguments positionnels, qui sont donc obligatoires
+    # parser.add_argument("date", help="Date du journal à générer, sous le format 'annee-mois-jour'")
+    # parser.add_argument("edition", help="Édition du journal à générer", choices=["national", "idf", "paca"])
+    # # Argparse nous renvoie ensuite un objet contenant les valeurs des arguments
+    # args = parser.parse_args()
 
-    # Instanciation du générateur et appel de ses méthodes
-    generateur = Generateur(args.date, args.edition)
-    elements = generateur.importer(articles, interviews)
-    generateur.afficher(elements)
+    # # Instanciation du générateur et appel de ses méthodes
+    # generateur = Generateur(args.date, args.edition)
+    # elements = generateur.importer(articles, interviews)
+    # generateur.afficher(elements)
